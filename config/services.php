02ca545c51999361c244fb2d95e107eb2da99295
SPMatthee <basmatthee@gmail.com>
2015-11-10 19:23:38 +0100

    Added private variable for the services in Template class.

--- config/services.php
+++ config/services.php
@@ -2,6 +2,11 @@
 /**
  * services.php
  *
+ * This is where the services are defined. It is also possible to use defined
+ * services as parameters. Once a service class has been instantiated, the same
+ * object is served from that moment. Classes will be instantiated only once and
+ * only if the service is used. Unused services will never be instantiated.
+ *
  * @author Bas Matthee <basmatthee@gmail.com>
  * @copyright Copyright (c) 2015 Bas Matthee <http://www.bas-matthee.nl>
  */
